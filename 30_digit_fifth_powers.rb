18fca84b499c718b6aa7b362a5cf99967846b9d0
natey37 <natey37@gmail.com>
2017-06-22 17:48:22 -0400

    Add files via upload

---
+++ 30_digit_fifth_powers.rb
@@ -0,0 +1,22 @@
+#Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+#1634 = 1*4 + 6^4 + 3^4 + 4^4
+#8208 = 8^4 + 2^4 + 0^4 + 8^4
+#9474 = 9^4 + 4^4 + 7^4 + 4^4
+#As 1 = 1^4 is not a sum it is not included.
+#The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+#Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+def digit_fifth_powers
+  fifth_power_nums = []
+# largest num is 6 digits long, max = 9**5 * 6 = 354294
+  (1..354294).each do |x|
+  sum = 0
+    x.to_s.split("").each do |x|
+      sum += x.to_i ** 5
+    end 
+    fifth_power_nums << x if x == sum unless x == 1
+  end 
+    return fifth_power_nums.reduce(:+)
+end 
+
+digit_fifth_powers => 443839
