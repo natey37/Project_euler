2684730a947b5245a5e515913057994522d2832e
natey37 <natey37@gmail.com>
2017-06-22 16:08:02 -0400

    Update 16_power_digit_sum.rb

--- 16_power_digit_sum.rb
+++ 16_power_digit_sum.rb
@@ -1,7 +1,10 @@
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+
 def power_digits(n)
-  sum = 2 ** 1000
+  sum = 2 ** n
   sum = sum.to_s.split("").map{|x| x.to_i}.reduce(:+)
-  return "The sum of the digits of 2 raised to the #{n} power is #{sum}"
+    return sum 
 end 
 
 power_digits(1000) => 1366
