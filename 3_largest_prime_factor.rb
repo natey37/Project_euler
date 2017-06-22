5b0ecde9b36099f7dae998e8362d611b76caede2
natey37 <natey37@gmail.com>
2017-06-22 12:30:01 -0400

    Update 3_largest_prime_factor.rb

--- 3_largest_prime_factor.rb
+++ 3_largest_prime_factor.rb
@@ -1,4 +1,5 @@
-#num is the number we are finding the largest prime factor for
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143 ?
 
 require 'prime'
 
