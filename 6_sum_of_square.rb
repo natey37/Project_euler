e936cb527fea7040ceaf6fe7557075fd07076c29
natey37 <natey37@gmail.com>
2017-06-22 12:42:58 -0400

    Update 6_sum_of_square.rb

--- 6_sum_of_square.rb
+++ 6_sum_of_square.rb
@@ -1,4 +1,11 @@
-def sum_square_different(upper_limit)
+#The sum of the squares of the first ten natural numbers is,
+#12 + 22 + ... + 102 = 385
+#The square of the sum of the first ten natural numbers is,
+#(1 + 2 + ... + 10)2 = 552 = 3025
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+def sum_square_difference(upper_limit)
   sum_of_squares = 0 
   upper_limit.downto(1) do |i|
     num_squared = i * i 
@@ -9,4 +16,4 @@ def sum_square_different(upper_limit)
     square_of_sum - sum_of_squares
 end 
 
-sum_square_different(100) => (25164150)
+sum_square_difference(100) => (25164150)
